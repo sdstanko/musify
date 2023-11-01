f78f782fbadd1dcd90d4b27ef2701de38a53f3f0
Daniel Stanko <stankodanijel@gmail.com>
2023-11-01 15:23:38 +0100

    try

--- client/components/HomeAlbumRandom/HomeAlbumRandom.tsx
+++ client/components/HomeAlbumRandom/HomeAlbumRandom.tsx
@@ -12,7 +12,9 @@ const HomeAlbumRandom: FC<IHomeAlbumRandomProps> = ({ album }) => {
     const router = useRouter();
 
     return (
-        <div className={styles.item} onClick={() => router.push(`/musify/album/${album._id}`)}>
+        <div className={styles.item} 
+        // onClick={() => router.push(`/musify/album/${album._id}`)}
+        >
             <div className={styles.item__cover}>
                 <img
                     className={styles.item__img}
