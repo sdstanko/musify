2555a9904c80214387931ae884d996783d6081c7
Daniel Stanko <stankodanijel@gmail.com>
2023-11-01 21:59:41 +0100

    mw

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,15 +1,32 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-    if (request.nextUrl.href === 'http://localhost:3000/album/653768818eb22bc7bf309fbc') {
-        // console.log(request.nextUrl.searchParams.entries()); 
+export function middleware(req: NextRequest): NextResponse | Response {
+	const domain = 'danijel.pro'
+
+    const { pathname } = req.nextUrl;
+	console.log(pathname)
+    if (
+        pathname.startsWith('/api') || //  exclude all API routes
+        pathname.startsWith('/static') || // exclude static files
+        pathname.includes('.') // exclude all files in the public folder
+    ) {
+        return NextResponse.next();
     }
-    // return NextResponse.redirect(new URL('/home', request.url));
-}
 
-// See "Matching Paths" below to learn more
-// export const config = {
-//     matcher: '/about/:path*',
-// };
+	
+    const hostname = req?.headers?.get('host');
+	console.log(hostname)
+
+    const whitelistDomain = [domain, `www.${domain}`];
+
+    if (hostname && !whitelistDomain.includes(hostname)) {
+        const subdomain = hostname?.split('.');
+        const url = req.nextUrl.clone();
+
+		console.log(`/store/${[subdomain[0], url.pathname].join('')}`);
+        return NextResponse.rewrite(req.nextUrl);
+        req.nextUrl.pathname = `/store/${[subdomain[0], url.pathname].join('')}`;
+    }
+
+    return NextResponse.next();
+}
