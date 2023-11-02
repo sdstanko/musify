a7f0377d121a133b13be379a621622a4fd9ad120
Daniel Stanko <stankodanijel@gmail.com>
2023-11-02 10:35:54 +0100

    fix

--- client/middleware.ts
+++ client/middleware.ts
@@ -2,11 +2,11 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest): NextResponse | Response {
 	if (req.headers.get('referer') === 'https://danijel.pro/musify') {
-		console.log(req)
-		console.log(NextResponse.next());
 	}
+	console.log(req)
+	console.log(NextResponse.next());
 
-	if (req.nextUrl.pathname.startsWith('/_next')) {
+	if (req.nextUrl.pathname.startsWith('/_next/data')) {
 
 	}
 	
