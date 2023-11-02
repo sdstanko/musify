c267957bb2e078d3626672953e0b3059a31a1c7d
Daniel Stanko <stankodanijel@gmail.com>
2023-11-02 17:51:22 +0100

    fix

--- client/next.config.js
+++ client/next.config.js
@@ -1,26 +1,26 @@
 const BASE_PREFIX_FOR_APP = '/musify'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: BASE_PREFIX_FOR_APP,
-  async rewrites(){
-    return [
-      {
-        /** ASSET PREFIX */
-        source: `${BASE_PREFIX_FOR_APP}/_next/:path*`,
-        destination: '/_next/:path*'
-      },
-      {
-        /** IMAGE PREFIX */
-        source: `${BASE_PREFIX_FOR_APP}/images/:query*`,
-        destination: '/_next/image/:query*'
-      },
-      /** API PREFIX */
-      {
-        source: `${BASE_PREFIX_FOR_APP}/api/:path*`,
-        destination: '/api/:path*'
-      }
-    ]
-  }
+  // assetPrefix: BASE_PREFIX_FOR_APP,
+  // async rewrites(){
+  //   return [
+  //     {
+  //       /** ASSET PREFIX */
+  //       source: `${BASE_PREFIX_FOR_APP}/_next/:path*`,
+  //       destination: '/_next/:path*'
+  //     },
+  //     {
+  //       /** IMAGE PREFIX */
+  //       source: `${BASE_PREFIX_FOR_APP}/images/:query*`,
+  //       destination: '/_next/image/:query*'
+  //     },
+  //     /** API PREFIX */
+  //     {
+  //       source: `${BASE_PREFIX_FOR_APP}/api/:path*`,
+  //       destination: '/api/:path*'
+  //     }
+  //   ]
+  // }
 };
 
 module.exports = nextConfig;
