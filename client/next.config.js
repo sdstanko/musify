57f960633dbd2734900a1377bbf0b03deb015324
Daniel Stanko <stankodanijel@gmail.com>
2023-11-02 22:27:55 +0100

    rewrite add

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/musify/album/:id',
+        destination: '/album/:id',
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
