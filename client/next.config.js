566885654d8b05fbe6391a2d2e7493505da75c9d
Daniel Stanko <stankodanijel@gmail.com>
2023-11-02 10:08:17 +0100

    fix config

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,15 @@
+const BASE_PREFIX_FOR_APP = '/musify'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
   // basePath: '/musify'
+
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return 'musify'
+  },
 };
 
 module.exports = nextConfig;
